ba0a1749bf4c5bfde5e825fdff5bf639b998d85a
KCH9231 <104981761+KCH9231@users.noreply.github.com>
2022-12-06 08:16:52 +0800

    Update bilibilicc.js

--- bilibilicc.js
+++ bilibilicc.js
@@ -14,7 +14,7 @@
 [rewrite_local]
 
 # ～ 嗶哩嗶哩繁体CC字幕转中文简体（2022-10-12）@ddgksf2013
-^https?:\/\/i.\.hdslb\.com\/bfs\/subtitle\/.+\.json$ url script-response-body https://github.com/ddgksf2013/Scripts/raw/main/bilibili_cc.js
+^https?:\/\/i.\.hdslb\.com\/bfs\/subtitle\/.+\.json$ url script-response-body https://raw.githubusercontent.com/KCH9231/jiaoben/main/bilibilicc.js
 
 [mitm]
 
